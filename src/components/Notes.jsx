d348954eff6f6f712336fd82eb726f57bb24dc05
Haritha Palat <97822104+HarithaPalat@users.noreply.github.com>
2022-12-28 13:12:19 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Deep Learning</h1>
+      <p>
+        Deep learning is part of a broader family of machine learning methods
+        based on artificial neural networks with representation learning.
+        Learning can be supervised, semi-supervised or unsupervised.
+      </p>
+    </div>
+  );
+}
+export default Notes;
